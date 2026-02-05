e0b3793fc5068da3a9c17533fcd1a1bdcac4f587
KONAIN-WORSI <konainworsi@gmail.com>
2026-02-05 23:00:07 +0545

    added the display feature in admin panel

--- wareHouse/warehouses/admin.py
+++ wareHouse/warehouses/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Warehouse, StorageLocation, StorageZone
 
 # Register your models here.
+@login_required
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'code',
+        'city',
+        'manager',
+        'phone',
+        'capacity_display',
+        'status_badge',
+        'is_active',
+        'created_at'
+    ]
+
+    list_filter = ['status', 'is_active','city','created_at']
+    search_fields = ['name','code','city','address']
+    prepopulated_fields = {'slug': ('name',)}
+
+    readonly_fields = ['created_at','updated_at','created_by']
+
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'code', 'slug', 'description')
+        }),
+        ('Location Details', {
+            'fields': ('address','city','state','postal_code','country')
+        }),
+        ('Contact Information', {
+            'fields': ('phone', 'email','manager')
+        }),
+        ('Capacity & Status', {
+            'fields': ('total_capacity','status','is_active')
+        }),
+        ('Metadata', {
+            'fields': ('created_at','updated_at','created_by'),
+            'classes': ('collapse',)
+        })
+    )
+
+    
+
+
+
+    
